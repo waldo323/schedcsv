6bde3a8cd632da86392e68941d84719c7c457f22
James <Jameshice@gmail.com>
2013-04-20 23:28:41 -0400

    increace automation of schedule output

---
+++ getsched.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+wget https://docs.google.com/spreadsheet/ccc?key=0AiCkD773xnvKdHZ5NU9OcThZNXVDZDBONXdVQjJ5T3c&output=csv --output-document sched.csv
+wget https://docs.google.com/spreadsheet/ccc?key=0AiCkD773xnvKdHZ5NU9OcThZNXVDZDBONXdVQjJ5T3c&output=xls --output-document sched.xlsx
