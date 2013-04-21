68a90e98e9b019f03923e5073fdac4411e251d61
James <Jameshice@gmail.com>
2013-04-21 01:36:07 -0400

    updated schedule to the lastest downloaded version as of Sun Apr 21 01:36:06 EDT 2013

--- getsched.sh
+++ getsched.sh
@@ -14,14 +14,14 @@ echo process the data
 python schedcsv.py
 echo looks good!
 
-# git add . for adding all changes
+## git add . for adding all changes
 echo staging to local repo
 git add .
-# git commit for committing those changes to the local repo
+## git commit for committing those changes to the local repo
 echo committing to local repo
 git commit -m "updated schedule to the lastest downloaded version as of $SCHEDDATE"
 
-# git push sends it up to github
+## git push sends it up to github
 echo pushing up to github
 git push
 
