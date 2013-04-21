c32ef0978350c51f74ddc9b2d6ce8b7f09a2ceef
James <Jameshice@gmail.com>
2013-04-20 23:55:24 -0400

    updated schedule to the lastest downloaded version as of Sat Apr 20 23:55:23 EDT 2013

--- getsched.sh
+++ getsched.sh
@@ -4,21 +4,26 @@
 #wget --output-documuent sched.csv "https://docs.google.com/spreadsheet/ccc?key=0AiCkD773xnvKdHZ5NU9OcThZNXVDZDBONXdVQjJ5T3c&output=csv"
 
 #rm ./sched.csv
-
+echo get the schedule from google drive
 wget --output-document sched.csv "https://docs.google.com/spreadsheet/ccc?key=0AiCkD773xnvKdHZ5NU9OcThZNXVDZDBONXdVQjJ5T3c&output=csv"
 
 SCHEDDATE=`date`
 
 # process schedule into its various output files
+echo process the data
 python schedcsv.py
-
+echo looks good!
 
 # git add . for adding all changes
+echo staging to local repo
 git add .
 # git commit for committing those changes to the local repo
+echo committing to local repo
 git commit -m "updated schedule to the lastest downloaded version as of $SCHEDDATE"
 
 # git push sends it up to github
+echo pushing up to github
 git push
 
 
+echo successful!
