7cbbc8fcaf37c6e7b65253a881322eb0bd9bd94e
James <Jameshice@gmail.com>
2013-04-21 21:02:52 -0400

    updated schedule to the lastest downloaded version as of Sun Apr 21 21:02:52 EDT 2013

--- getsched.sh
+++ getsched.sh
@@ -14,7 +14,7 @@ echo process the data
 python schedcsv.py
 echo looks good!
 
-#post process the speakerlist
+echo post process the speakerlist
 awk '{x=$NF; $NF=""; print x ", " $0 }' ./output/2013.penguicon.speakers.3plus.txt |sort > ./output/2013.penguicon.spkrs.bylast.3plus.txt 
 
 ## git add . for adding all changes
