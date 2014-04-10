13e84254d126f8faac5cbb81035c175ab818ecf5
James H <jameshicegmail.com>
2014-04-10 17:35:56 -0400

    updated schedule to the lastest downloaded version as of Thu Apr 10 17:35:56 EDT 2014

--- getrunpush.sh
+++ getrunpush.sh
@@ -6,5 +6,5 @@ echo get schedule and run the parsing script on the schedule
 ./getsched.sh
 
 
-# push the schedule and output files to github
-./pushsched.sh
+# push the schedule and output files to github ./pushsched.sh
+git status|awk '/Changes/&&/not/&&/staged/{system("./pushsched.sh")}'
