d4f8c62a27e6e0a89daa5386bbe16b963d95096a
waldo323 <jameshice@gmail.com>
2014-04-15 15:18:48 -0400

    Update getrunpush.sh

--- getrunpush.sh
+++ getrunpush.sh
@@ -4,7 +4,7 @@ git pull
 
 echo get schedule and run the parsing script on the schedule 
 ./getsched.sh
-
-
+echo time and date of task
+date
 # push the schedule and output files to github ./pushsched.sh
 git status|awk '/Changes/&&/not/&&/staged/{system("./pushsched.sh")}'
