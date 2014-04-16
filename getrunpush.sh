57f3f6c8be6815a529be8c2a7c75583e605d40e2
James Hice <jameshice@linux.com>
2014-04-16 12:05:38 -0400

    updated schedule to the lastest downloaded version as of Wed Apr 16 12:05:38 EDT 2014

--- getrunpush.sh
+++ getrunpush.sh
@@ -7,10 +7,12 @@ git rm ./output/2014.*
 git rm ./output/schedbyspeaker/*
 git rm ./output/schedbyroom/*
 git rm ./output/schedsbyspeaker.zip
-
-
 echo get schedule and run the parsing script on the schedule 
 ./getsched.sh
+
+# zip up the speakers' csv import to ical files
+zip ./output/schedsbyspeaker.zip ./output/schedbyspeaker/*
+git add ./output/schedsbyspeaker.zip
 echo time and date of task
 date
 # push the schedule and output files to github ./pushsched.sh
