eac7b2a6193d55f9519e5af70b36327a8571f5b8
James H <jameshicegmail.com>
2014-04-16 16:16:56 -0400

    updated schedule to the lastest downloaded version as of Wed Apr 16 16:16:56 EDT 2014

--- getrunpush.sh
+++ getrunpush.sh
@@ -15,7 +15,7 @@ cp penguicon.schedule.xml ./output/penguicon.schedule.xml
 # zip up the speakers' csv import to ical files
 zip ./output/schedsbyspeaker.zip ./output/schedbyspeaker/*
 git add ./output/schedsbyspeaker.zip
-echo time and date of task
+echo time and date of task `date>> ../logs/getrunpushcron.log` 
 date
 # push the schedule and output files to github ./pushsched.sh
 git status|awk '/Changes/&&/not/&&/staged/{system("./pushsched.sh")}'
