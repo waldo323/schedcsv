b5114241b84c7e7a82b347ca3a788c56e98a10df
James Hice <jameshice@gmail.com>
2016-03-31 23:01:30 -0400

    typo

--- getsched.sh
+++ getsched.sh
@@ -26,7 +26,7 @@ SCHEDDATE=`date`
 # process schedule into its various output files
 echo process the data
 python schedcsv.py sched.csv && echo looks good!
-pythoh full_sched_web_template_filler.py > full_current_data_for_website.html 
+#python full_sched_web_template_filler.py > full_current_data_for_website.html 
 
 echo post process the speakerlists
 awk '{x=$NF; $NF=""; print x ", " $0 }' ./output/2016.penguicon.speakers.3plus.txt |sort > ./output/2016.penguicon.spkrs.bylast.3plus.txt 
