7f0b436e439dfa0bc776d9c5b53b506a350c4a53
James Hice <jameshice@gmail.com>
2019-04-12 04:33:16 -0400

    updated schedule to the lastest downloaded version as of Fri Apr 12 04:33:16 EDT 2019

--- getsched.sh
+++ getsched.sh
@@ -33,8 +33,8 @@ python schedcsv.py sched.csv && echo looks good!
 #python full_sched_web_template_filler.py > full_current_data_for_website.html 
 
 echo post process the speakerlists
-#awk '{x=$NF; $NF=""; print x ", " $0 }' ./output/2019.penguicon.speakers.3plus.txt |sort > ./output/2019.penguicon.spkrs.bylast.3plus.txt 
+awk '{x=$NF; $NF=""; print x ", " $0 }' ./output/2019.penguicon.speakers.3plus.txt |sort > ./output/2019.penguicon.spkrs.bylast.3plus.txt 
 
-#awk '{x=$NF; $NF=""; print x ", " $0 }' ./output/2019.penguicon.speakers.txt |sort> ./output/2019.penguicon.spkrs.bylast.txt 
+awk '{x=$NF; $NF=""; print x ", " $0 }' ./output/2019.penguicon.speakers.txt |sort> ./output/2019.penguicon.spkrs.bylast.txt 
 
 echo got schedule and ran parsing script
