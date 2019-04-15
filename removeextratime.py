ea91b37c66fef6223a6020ca1f030871cbaee777
James Hice <jameshice@gmail.com>
2019-04-14 21:20:30 -0400

    updated schedule to the lastest downloaded version as of Sun Apr 14 21:20:30 EDT 2019

--- removeextratime.py
+++ removeextratime.py
@@ -12,8 +12,8 @@ for index, row in enumerate(inputfile):
         if currenttime != lasttime:
             outputfile.write(row)
             lasttime = row
-        if "<day>All Weekend</day>" in inputfile[index-1]:
-            outputfile.write("<time>All Weekend</time>")
+#        if "<day>All Weekend</day>" in inputfile[index-1]:
+#            outputfile.write("<time>All Weekend</time>")
     else: # If this is not a time row
         outputfile.write(row)
 
