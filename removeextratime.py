59d8bb89b6e27a4aba84b5caa9e2edae84782b7d
James Hice <jameshice@gmail.com>
2019-04-14 21:14:35 -0400

    updated schedule to the lastest downloaded version as of Sun Apr 14 21:14:35 EDT 2019

--- removeextratime.py
+++ removeextratime.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
+from operator import itemgetter
 inputfile = open('penguicon.schedule.xml', 'r') 
 outputfile = open('2019.penguicon.final.schedule.xml', 'w')
 
-currenttime = '<time>12 PM</time>'
+currenttime = '<time>4 PM</time>'
 lasttime = '<time>All Weekend</time>'
 
-for row in inputfile:
+for index, row in enumerate(inputfile):
     if '<time>' in row:
         currenttime = row
         if currenttime != lasttime:
             outputfile.write(row)
             lasttime = row
+        if inputfile[index-1] == "<day>All Weekend</day>":
+            outputfile.write(<time>All Weekend</time>)
     else: # If this is not a time row
         outputfile.write(row)
 
