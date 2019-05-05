c9351e2cdfce739b1ce0c608f9c998fdf0c52273
James Hice <jameshice@gmail.com>
2019-05-05 17:10:38 -0400

    updated schedule to the lastest downloaded version as of Sun May  5 17:10:38 EDT 2019

--- getrunpush.sh
+++ getrunpush.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
 echo update repo first!
 git pull
-
+CWD="/home/jhice/projects/schedcsv"
 # clean up previous output files
 #git rm ./output/2014.*
 #git rm ./output/2015.*
-git rm ./output/schedbyspeaker/*.csv
-git rm ./output/schedbyroom/*
-git rm ./output/schedsbyspeaker.zip
+#git rm ./output/2016.*
+#git rm ./2016.*
+git rm $CWD/output/schedbyspeaker/*.csv
+git rm $CWD/output/schedbyroom/*
+git rm $CWD/output/schedsbyspeaker.zip
 #git rm ./output/calendars/*
 echo get schedule and run the parsing script on the schedule 
-./getsched.sh
-./removeextratime.py
+$CWD/getsched.sh 5ea81a81d7732d9c00787a401fce3d3a
+$CWD/removeextratime.py
 cp 2019.penguicon.final.schedule.xml penguicon.schedule.xml
+
 # make it easier to find the schedule
-cp penguicon.schedule.xml ./output/penguicon.schedule.xml 
+cp $CWD/penguicon.schedule.xml $CWD/output/penguicon.schedule.xml 
 # copy to final schedule
 #cp penguicon.schedule.xml 2019.penguicon.final.schedule.xml 
 # zip up the speakers' csv import to ical files
-cd ./output/schedbyspeaker && zip -j ../schedsbyspeaker.zip ./* && cd ../../
+cd $CWD/output/schedbyspeaker && zip -j ../schedsbyspeaker.zip ./* && cd ../../
 #git add ./output/schedsbyspeaker.zip
 #git add -A
 echo time and date of task  
 date
 # push the schedule and output files to github ./pushsched.sh
 git status|awk '/Changes/&&/not/&&/staged/{system("./pushsched.sh")}'
-#./pushsched.sh
+$CWD/pushsched.sh
